432318e33cfce6d05be8003d9b9b095b6dc03ab5
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-13 21:13:40 +0530

    created Student class

---
+++ src/tasks/task1/code/Student.java
@@ -0,0 +1,14 @@
+/*
+ * Author Name: Aditya Chaurasia
+ * Date: 13-09-2022
+ * Created With: IntelliJ IDEA Community Edition
+ * Profile: github.com/ChaurasiaAditya
+ */
+package tasks.task1.code;
+
+public class Student {
+    private byte rollNumber;
+    private String firstName;
+    private String lastName;
+    private byte currentYear;
+}
