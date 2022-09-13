ed5c09bc70aba8b56987d4326cde79ec93a7f194
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-14 01:28:47 +0530

    created createUserName method

--- src/tasks/task1/code/PortalCredentialsCreator.java
+++ src/tasks/task1/code/PortalCredentialsCreator.java
@@ -6,12 +6,25 @@
  */
 package tasks.task1.code;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PortalCredentialsCreator {
 
     public List<UserName> createListOfUserNames(List<Student> studentsList) {
+
+
         return null;
+    }
+
+    public UserName createUserName(Student student){
+        String firstName = student.getFirstName();
+        String lastName = student.getLastName();
+        byte currentYear = student.getCurrentYear();
+        byte rollNumber = student.getRollNumber();
+
+        String format = String.format("%s.%s.%d.%d", firstName, lastName, currentYear, rollNumber);
+        return new UserName(format);
     }
 
     public List<PassWord> createListOfRandomPasswords(List<Student> studentsList) {
