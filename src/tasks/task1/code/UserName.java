6ba432e83f16bdf482106dcdcbf6aa8d798977b5
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-14 01:40:23 +0530

    override equal and hasCode methods in UserName

--- src/tasks/task1/code/UserName.java
+++ src/tasks/task1/code/UserName.java
@@ -18,9 +18,18 @@ public class UserName {
     public UserName() {
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserName userName1 = (UserName) o;
+        return Objects.equals(userName, userName1.userName);
+    }
 
-
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(userName);
+    }
 
 
 }
