5212a5cb14fe0b5efae89f2041c7b2b5873d6546
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-13 21:37:20 +0530

    created PortalCredentialsCreator class

---
+++ src/tasks/task1/code/PortalCredentialsCreator.java
@@ -0,0 +1,10 @@
+/*
+ * Author Name: Aditya Chaurasia
+ * Date: 13-09-2022
+ * Created With: IntelliJ IDEA Community Edition
+ * Profile: github.com/ChaurasiaAditya
+ */
+package tasks.task1.code;
+
+public class PortalCredentialsCreator {
+}
