143df17c4a8ad0449cfbbd9622091a44e7287fc7
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-15 00:22:07 +0530

    created Test Result generator class

---
+++ src/tasks/task2/code/TestResultGenerator.java
@@ -0,0 +1,10 @@
+/*
+ * Author Name: Aditya Chaurasia
+ * Date: 15-09-2022
+ * Created With: IntelliJ IDEA Community Edition
+ * Profile: github.com/ChaurasiaAditya
+ */
+package tasks.task2.code;
+
+public class TestResultGenerator {
+}
