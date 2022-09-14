0cb16fa980e4ba6306a5c5ce3acc0fed1003adab
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-15 00:23:58 +0530

    created get result of failed student method

--- src/tasks/task2/code/TestResultGenerator.java
+++ src/tasks/task2/code/TestResultGenerator.java
@@ -6,5 +6,11 @@
  */
 package tasks.task2.code;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class TestResultGenerator {
+    public HashSet<String> getDetailsOfFailedStudents(Set<Student> studentSet) {
+        return null;
+    }
 }
