24dc36847d20c64a502b0eeb8450c4efc0d15c7b
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-14 22:50:18 +0530

    created equal, hashCode and to String Method in PassWord class

--- src/tasks/task1/code/PassWord.java
+++ src/tasks/task1/code/PassWord.java
@@ -6,6 +6,32 @@
  */
 package tasks.task1.code;
 
+import java.util.Objects;
+
 public class PassWord {
     private String passWord;
+
+    public PassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PassWord passWord1 = (PassWord) o;
+        return Objects.equals(passWord, passWord1.passWord);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(passWord);
+    }
+
+    @Override
+    public String toString() {
+        return "PassWord{" +
+                "passWord='" + passWord + '\'' +
+                '}';
+    }
 }
