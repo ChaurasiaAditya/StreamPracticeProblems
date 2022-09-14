a9f8f7834cbdeee8eba6394ce5587c8477c011b6
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-14 23:34:57 +0530

    Created Student Objects in Main Method

---
+++ src/tasks/task1/main/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Author Name: Aditya Chaurasia
+ * Date: 14-09-2022
+ * Created With: IntelliJ IDEA Community Edition
+ * Profile: github.com/ChaurasiaAditya
+ */
+package tasks.task1.main;
+
+import tasks.task1.code.PortalCredentialsCreator;
+import tasks.task1.code.Student;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Create Student Objects
+        Student student1 = new Student((byte) 1, "Aditya", "Chaurasia", (byte) 22);
+        Student student2 = new Student((byte) 2, "Divyansh", "Bharadwaj", (byte) 22);
+        Student student3 = new Student((byte) 3, "Pinky", "Sodhi", (byte) 22);
+    }
+}
