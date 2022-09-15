683b1dc68763e624544e8526757972cec619f72f
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-16 00:20:06 +0530

    created Employee class

---
+++ src/tasks/task3/code/Employee.java
@@ -0,0 +1,10 @@
+/*
+ * Author Name: Aditya Chaurasia
+ * Date: 16-09-2022
+ * Created With: IntelliJ IDEA Community Edition
+ * Profile: github.com/ChaurasiaAditya
+ */
+package tasks.task3.code;
+
+public class Employee {
+}
