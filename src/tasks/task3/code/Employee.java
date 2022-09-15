af4d693723636a16404aa768df46d8ca1f36176c
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-16 00:23:54 +0530

    created private final attributes and parameterized constructor

--- src/tasks/task3/code/Employee.java
+++ src/tasks/task3/code/Employee.java
@@ -7,4 +7,16 @@
 package tasks.task3.code;
 
 public class Employee {
+    private final short id;
+    private final String name;
+    private final double salary;
+    private final byte numberOfLeaves;
+
+    public Employee(short id, String name, double salary, byte numberOfLeaves) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.numberOfLeaves = numberOfLeaves;
+    }
+
 }
