37d1bc8b56741f85fd44b265000903d52c6780d3
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-15 23:30:33 +0530

    Added java docs in Task1

--- src/tasks/task1/code/PortalCredentialsCreator.java
+++ src/tasks/task1/code/PortalCredentialsCreator.java
@@ -11,6 +11,13 @@ import java.util.stream.Collectors;
 
 public class PortalCredentialsCreator {
 
+    /**
+     * "Create a list of UserName objects from a list of Student objects."
+     * The function is a one-liner, but it's still easy to understand
+     *
+     * @param studentsList The list of students that we want to convert to a list of UserNames.
+     * @return A list of UserName objects.
+     */
     public List<UserName> createListOfUserNames(List<Student> studentsList) {
 
         return studentsList
@@ -19,6 +26,12 @@ public class PortalCredentialsCreator {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * It creates a username for a student
+     *
+     * @param student The object of the class Student.
+     * @return A UserName object is being returned.
+     */
     private UserName createUserName(Student student){
         String firstName = student.firstName();
         String lastName = student.lastName();
@@ -28,6 +41,12 @@ public class PortalCredentialsCreator {
         return new UserName(username);
     }
 
+    /**
+     * Create a list of passwords from a list of students by mapping each student to a password.
+     *
+     * @param studentsList a list of students
+     * @return A list of PassWord objects.
+     */
     public List<PassWord> createListOfRandomPasswords(List<Student> studentsList) {
         return studentsList
                 .stream()
@@ -35,6 +54,12 @@ public class PortalCredentialsCreator {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * It creates a random string of 8 characters, and then concatenates the first name of the student to the end of it
+     *
+     * @param student the student object that we are creating the password for
+     * @return A string of 8 random letters and the student's first name.
+     */
     private String createStringPassword(Student student){
         int leftLimit = 97; // letter 'a'
         int rightLimit = 122; // letter 'z'
