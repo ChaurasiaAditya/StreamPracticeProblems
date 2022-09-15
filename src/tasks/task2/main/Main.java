53c080bc0f336b801fc745acda29b6bbd813c281
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-15 23:25:48 +0530

    created Main class in task 2

---
+++ src/tasks/task2/main/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Author Name: Aditya Chaurasia
+ * Date: 15-09-2022
+ * Created With: IntelliJ IDEA Community Edition
+ * Profile: github.com/ChaurasiaAditya
+ */
+package tasks.task2.main;
+
+import tasks.task2.code.Student;
+import tasks.task2.code.TestResultGenerator;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+}
