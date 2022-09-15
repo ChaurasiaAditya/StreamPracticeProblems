a8618ba41f99ea0802a0a760a755f316b8421446
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-15 23:26:39 +0530

    created student object in Main class

--- src/tasks/task2/main/Main.java
+++ src/tasks/task2/main/Main.java
@@ -15,5 +15,11 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
+
+        // create student object
+        Student student1 = new Student((byte) 1,"Divyansh",1234657890,98);
+        Student student2 = new Student((byte) 2,"Vaibhav",1234657890,28);
+        Student student3 = new Student((byte) 3,"Neeraj",1234657890,23);
+        Student student4 = new Student((byte) 4,"Aditya",1234657890,93);
     }
 }
