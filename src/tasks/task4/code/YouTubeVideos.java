602874d3273085556a4f9e16401f176bfb91c0b8
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-17 20:22:02 +0530

    added YouTubeVideos class

---
+++ src/tasks/task4/code/YouTubeVideos.java
@@ -0,0 +1,10 @@
+/*
+ * Author Name: Aditya Chaurasia
+ * Date: 17-09-2022
+ * Created With: IntelliJ IDEA Community Edition
+ * Profile: github.com/ChaurasiaAditya
+ */
+package tasks.task4.code;
+
+public class YouTubeVideos {
+}
