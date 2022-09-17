a5566f463d8b4ffef0e4f18a19cd431f7d7a3241
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-17 20:26:32 +0530

    added fields in YouTubeVideos class and added the constructor

--- src/tasks/task4/code/YouTubeVideos.java
+++ src/tasks/task4/code/YouTubeVideos.java
@@ -6,5 +6,18 @@
  */
 package tasks.task4.code;
 
+import java.math.BigInteger;
+
 public class YouTubeVideos {
+    private final long videoId;
+    private final String nameOfTheVideo;
+    private BigInteger numberOfLikes;
+    private int videoInMinutes;
+
+    public YouTubeVideos(long videoId, String nameOfTheVideo, BigInteger numberOfLikes, int videoInMinutes) {
+        this.videoId = videoId;
+        this.nameOfTheVideo = nameOfTheVideo;
+        this.numberOfLikes = numberOfLikes;
+        this.videoInMinutes = videoInMinutes;
+    }
 }
